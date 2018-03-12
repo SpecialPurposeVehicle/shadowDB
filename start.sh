e0ec2645116f2377625fe9215fc3026c5123ce63
Tom Keene <tom@theanthillsocial.co.uk>
2018-03-12 10:20:41 +0000

    Test start script

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 # Example start script
 # Change the machine/apppath to target your own computer
 machine=$(uname -n)
-apppath='/srv/meteor/shadow.db-estate.co.uk/'
 if [ $machine == 'dellboy' ]
 then
     NODE_ENV="development" npm start
 else
+    apppath='/srv/meteor/shadow.db-estate.co.uk/'
     forever start -c 'NODE_ENV="production" npm start' $apppath
 fi
 echo $NODE_ENV
